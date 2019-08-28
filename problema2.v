8cac8dfe2d86d48c89e033ea5070f481eb7631ef
lucamps <lucas.campos2@ufv.br>
2019-08-27 23:24:56 -0300

    Concluindo parte do pdf

--- problema2.v
+++ problema2.v
@@ -21,10 +21,8 @@ module testbench();
     S_autoNivel teste2(x, S2);
 
     initial begin
-        x = 4'b0000; #1;
-        
+        x = 4'b0000; #1;     
         $monitor("%d     x %b          S1 %b  S2 %b", i, x, S1, S2);  //impressao
-        
         for (i=1; i<16; i=i+1) begin
             x = x + 8'b0001; #1;   
             if ( S1 != S2  ) $display("FALHA");
