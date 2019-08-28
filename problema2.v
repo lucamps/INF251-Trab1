752cf8a9aeaeb2fcd097133fa3f1ece4ecc9ff76
lucamps <lucas.campos2@ufv.br>
2019-08-27 23:28:03 -0300

    comentando parte de impressão -> desnecessária

--- problema2.v
+++ problema2.v
@@ -22,7 +22,7 @@ module testbench();
 
     initial begin
         x = 4'b0000; #1;     
-        $monitor("%d     x %b          S1 %b  S2 %b", i, x, S1, S2);  //impressao
+        //$monitor("%d     x %b          S1 %b  S2 %b", i, x, S1, S2);  //impressao
         for (i=1; i<16; i=i+1) begin
             x = x + 8'b0001; #1;   
             if ( S1 != S2  ) $display("FALHA");
