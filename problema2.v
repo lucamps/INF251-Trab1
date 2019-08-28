7cbcb3dedec64bb909ae7f6c6d59eea9087283d1
lucamps <lucas.campos2@ufv.br>
2019-08-27 22:33:12 -0300

    Finalizando segundo problema (S - complemento de 2)

--- problema2.v
+++ problema2.v
@@ -7,34 +7,27 @@ module S_baixoNivel (input [3:0] x, output [4:0] S);
     assign S[0] = (~x[3] & x[2] & ~x[0]) | (~x[3] & x[1] & ~x[0]) | (x[3] & x[0]);
 endmodule
 
-/*module S_autoNivel (input [3:0] x, output [4:0] S);
-    if (x<0) begin
-        S = x-2;
-    end 
-    else begin
-        S = x+3;
-    end
-endmodule*/
+module S_autoNivel (input [3:0] x, output [4:0] S);
+    assign S = (x[3] == 1)? ((x-2)+5'b10000): (x+3);    
+endmodule
 
 //testbench
 module testbench();
     reg [3:0] x;
-    wire [4:0] S;
+    wire [4:0] S1,S2;
     integer i;
 
-    S_baixoNivel teste(x, S);
+    S_baixoNivel teste(x, S1);
+    S_autoNivel teste2(x, S2);
 
     initial begin
         x = 4'b0000; #1;
         
-        $monitor("%d     x %b          S %b", i, x, S);  //impressao
+        $monitor("%d     x %b          S1 %b  S2 %b", i, x, S1, S2);  //impressao
         
         for (i=1; i<16; i=i+1) begin
-            x = x + 8'b0001; #1;
-            
-            //if ( (a>b & M==0) | (a==b & I==0) | (a<b & (M==1 | I == 1)) ) $display("FALHA");
-            //Caso o programa entre em algumas destas condicoes, aparecera' uma mensagem de erro
-            
+            x = x + 8'b0001; #1;   
+            if ( S1 != S2  ) $display("FALHA");
         end
     end
 endmodule
